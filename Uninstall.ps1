ae61bedf70a64f99118b3f46e769b76198b6f143
NietThomas <thomas@tvhr.be>
2025-03-17 17:50:29 +0100

    Add files via upload
    Changed

--- Uninstall.ps1
+++ Uninstall.ps1
@@ -2,12 +2,14 @@
 .Synopsis
 Created on:   31/12/2021
 Created by:   Ben Whitmore
-Filename:     Remove-Printer.ps1
+Modified on:  17/03/2025
+Modified by:  Thomas Vanhooren
+Filename:     Uninstall.ps1
 
-powershell.exe -executionpolicy bypass -file .\Remove-Printer.ps1 -PrinterName "Canon Printer Upstairs"
+powershell.exe -executionpolicy bypass -file .\Uninstall.ps1 -PrinterName "Canon Printer Upstairs"
 
 .Example
-.\Remove-Printer.ps1 -PrinterName "Canon Printer Upstairs"
+.\Uninstall.ps1 -PrinterName "Canon Printer Upstairs"
 #>
 
 [CmdletBinding()]
@@ -15,15 +17,18 @@ Param (
     [Parameter(Mandatory = $True)]
     [String]$PrinterName
 )
-
+# Uninstalls the printer
 Try {
-    #Remove Printer
+    # Remove Printer
     $PrinterExist = Get-Printer -Name $PrinterName -ErrorAction SilentlyContinue
+    $registryPath = "HKLM:\SOFTWARE\IntunePrinters\" + $PrinterName
     if ($PrinterExist) {
         Remove-Printer -Name $PrinterName -Confirm:$false
+        Get-Item $registryPath | Remove-Item -Force -Verbose
+    } else {
+        Write-Warning "Printer '$PrinterName' does not exist."
     }
 }
 Catch {
-    Write-Warning "Error removing Printer"
-    Write-Warning "$($_.Exception.Message)"
+    Write-Warning "Error removing Printer: $($_.Exception.Message)"
 }
